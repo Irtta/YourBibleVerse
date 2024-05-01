962f61ecb9036f84c808cca28ce18fd3b3effd0d
Irtta <145128291+Irtta@users.noreply.github.com>
2024-05-01 18:44:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 // Function to execute when the DOM content is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    // Check if there is a token in the URL parameters
-    const token = (new URLSearchParams(window.location.search)).get('token');
+    // Check if the user is already authenticated
+    const isAuthenticated = sessionStorage.getItem('authenticated') === 'true';
 
-    // Log token for debugging
-    console.log("Token from URL:", token);
-
-    // If a token is present in the URL
-    if (token) {
-        // Attempt to authenticate the token
-        authenticateToken(token);
+    // If the user is authenticated
+    if (isAuthenticated) {
+        // Redirect to clean URL (remove token from URL)
+        redirectToCleanURL();
     } else {
-        // If no token is found in the URL, display authentication failure message
-        displayAuthenticationMessage();
+        // Check if there is a token in the URL parameters
+        const token = (new URLSearchParams(window.location.search)).get('token');
+
+        // If a token is present in the URL
+        if (token) {
+            // Attempt to authenticate the token
+            authenticateToken(token);
+        } else {
+            // If no token is found in the URL, display authentication failure message
+            displayAuthenticationMessage();
+        }
     }
 });
 
@@ -21,16 +27,13 @@ function authenticateToken(token) {
     // Expected authentication token
     const expectedToken = "sY6gXmTb8qYnJxMw8qAs5lFvJmO6tGpP9ySfZhHtUw0qW$zEcNw9yR!g";
     
-    // Log expected token for debugging
-    console.log("Expected Token:", expectedToken);
-
     // If the provided token matches the expected token
     if (token === expectedToken) {
-        // Log authentication success for debugging
-        console.log("Authentication successful");
-
         // Store authentication status in session storage
         sessionStorage.setItem('authenticated', 'true');
+        
+        // Redirect to clean URL (remove token from URL)
+        redirectToCleanURL();
     } else {
         // If authentication fails, display authentication failure message
         displayAuthenticationMessage();
@@ -40,5 +43,14 @@ function authenticateToken(token) {
 // Function to display authentication failure message
 function displayAuthenticationMessage() {
     // Display a message indicating authentication failure
-    console.log("Authentication failed. Please tap your NFC card again.");
+    document.getElementById('verseDisplay').innerHTML = "Authentication failed. Please tap your NFC card again.";
+}
+
+// Function to redirect to a clean URL (remove token from URL)
+function redirectToCleanURL() {
+    // Clean URL
+    const cleanURL = "https://irtta.github.io/YourBibleVerse/";
+
+    // Redirect to the clean URL
+    window.location.href = cleanURL;
 }
